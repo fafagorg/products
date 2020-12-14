1f179d793949d653c04adbc2cff6dd9c57a773c2
alfobralla <47741431+alfobralla@users.noreply.github.com>
2020-12-14 10:33:47 +0000

    added products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:String,
+    price: Number,
+    seller: Number, //id del vendedor
+    id: Number
+});
+
+productSchema.methods.cleanup = function(){
+    return {name: this.name, price: this.price, seller: this.seller};
+}
+
+const Product = mongoose.model('Product',productSchema);
+
+module.exports = Product;
