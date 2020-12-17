19eba957389b84944cf3852675a6bbbecc5d45bb
pabgarbar <ezdes23@hotmail.com>
2020-12-17 15:23:52 +0100

    added category to products

--- products.js
+++ products.js
@@ -4,11 +4,12 @@ const productSchema = new mongoose.Schema({
     name:String,
     price: Number,
     seller: Number, //id del vendedor
+    category: String,
     id: Number
 });
 
 productSchema.methods.cleanup = function(){
-    return {name: this.name, price: this.price, seller: this.seller};
+    return {name: this.name, price: this.price, seller: this.seller, category: this.category};
 }
 
 const Product = mongoose.model('Product',productSchema);
