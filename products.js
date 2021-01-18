7e0c9bf64ecd1aa4f9ed217842696fdc9ea68e57
alfobralla <47741431+alfobralla@users.noreply.github.com>
2021-01-18 16:31:17 +0100

    fixed productName to unique

--- products.js
+++ products.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema({
 
     name: {
         type: String,
-        required: true
+        required: true,
+        unique:true
       },
     category: {
         type: String,
