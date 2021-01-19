7a3f076e2adfbf2527b243e02fa5f3a816afb105
pabgarbar <ezdes23@hotmail.com>
2021-01-19 12:36:03 +0100

    filter version 2

--- controllers/productsControllerService.js
+++ controllers/productsControllerService.js
@@ -53,92 +53,35 @@ module.exports.findProducts = function findProducts(req, res, next) {
           console.log(Date() + "-"+err);
           res.sendStatus(500);
       }else{
-          if (keyWord && productCategory && min_price && max_price){
-              console.log("ALL PARAMS")
-              var r = products.filter(p => p.price >= min_price && p.price <= max_price && p.category == productCategory && p.name.includes(keyWord));
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
+          var r = products;
+
+          if (keyWord){
+              console.log("Keyword");
+            r =  r.filter(p => p.name.includes(keyWord));
           }
-          else if (keyWord != undefined){
-              console.log("ONLY KEYWORD");
-              var r = products.filter(p => p.name.includes(keyWord));
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
+          if (productCategory){
+            console.log("Category");
+            r = r.filter(p => p.category == productCategory);
           }
-          else if (productCategory != undefined){
-              console.log("ONLY CATEGORY");
-              var r = products.filter(p => p.category == productCategory);
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
+          if (min_price){
+            console.log("Min Price");
+            r = r.filter(p => p.price >= min_price);
           }
-          else if (min_price && max_price){
-              console.log("BOTH PRICES")
-              var r = products.filter(p => (p.price >= min_price && p.price <= max_price));
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
+          if (max_price){
+            console.log("Max price");
+            r = r.filter(p => p.price <= max_price);
           }
-          else if (min_price != undefined){
-              console.log("ONLY MIN PRICE");
-              var r = products.filter(p => (p.price >= min_price));
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
-          }
-          else if (max_price != undefined){
-              console.log("ONLY MAX PRICE");
-              var r = products.filter(p => (p.price <= max_price));
-              if (r.length > 0){
-                res.send(r.map((product)=>{
-                  return product.cleanup();
-                }))
-              }
-              else{
-                res.status(404).send("not found");
-              }
-          }
-          else{
-            console.log("NO PARAMS")
-            if(products.length > 0){
-              res.send(products.map((product)=>{
-                return product.cleanup();
-              }))
-            }else{
-              res.status(404).send("There aren't products in the store");
-            }
-          };
+
+
+          res.send(r.map((product)=>{
+            return product.cleanup();
+          }))
+
       }
   });
 });
 };
+
 
 module.exports.addProduct = function addProduct(req, res, next) {
   var product = req.undefined.value;
