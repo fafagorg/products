1fdc51ceeb84f8039380ea43ca5c6c1983ed94fb
pabgarbar <ezdes23@hotmail.com>
2021-01-21 11:40:26 +0100

    changed rates call in controller sevice

--- controllers/ratesControllerService.js
+++ controllers/ratesControllerService.js
@@ -6,8 +6,8 @@ const ExchangeResource = require('../resources/exchangeResource.js')
 module.exports.getRates = function getRates(req, res, next) {
 
   ExchangeResource.requestExchange().then((response) =>{
-
-    var divisas = Object.keys(response.rates).map((key) => [key, response.rates[key]]);
+    var divisas = []
+    Object.keys(response.rates).forEach((key) => divisas.push({label: key, value: response.rates[key]}));
     res.send(divisas);
     
   })
